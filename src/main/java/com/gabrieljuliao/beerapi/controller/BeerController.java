39c55ae0014fd7a2f206e67ea3826d6f747dbacd
gabrieljuliao <gabrielbjuliao@gmail.com>
2021-12-10 11:55:13 -0300

    Controller and Swagger config.

---
+++ src/main/java/com/gabrieljuliao/beerapi/controller/BeerController.java
@@ -0,0 +1,50 @@
+package com.gabrieljuliao.beerapi.controller;
+
+import lombok.AllArgsConstructor;
+import com.gabrieljuliao.beerapi.dto.BeerDTO;
+import com.gabrieljuliao.beerapi.dto.QuantityDTO;
+import com.gabrieljuliao.beerapi.exception.BeerAlreadyRegisteredException;
+import com.gabrieljuliao.beerapi.exception.BeerNotFoundException;
+import com.gabrieljuliao.beerapi.exception.BeerStockExceededException;
+import com.gabrieljuliao.beerapi.service.BeerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/beers")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class BeerController implements BeerControllerDocs {
+
+    private final BeerService beerService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BeerDTO createBeer(@RequestBody @Valid BeerDTO beerDTO) throws BeerAlreadyRegisteredException {
+        return beerService.createBeer(beerDTO);
+    }
+
+    @GetMapping("/{name}")
+    public BeerDTO findByName(@PathVariable String name) throws BeerNotFoundException {
+        return beerService.findByName(name);
+    }
+
+    @GetMapping
+    public List<BeerDTO> listBeers() {
+        return beerService.listAll();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws BeerNotFoundException {
+        beerService.deleteById(id);
+    }
+
+    @PatchMapping("/{id}/increment")
+    public BeerDTO increment(@PathVariable Long id, @RequestBody @Valid QuantityDTO quantityDTO) throws BeerNotFoundException, BeerStockExceededException {
+        return beerService.increment(id, quantityDTO.getQuantity());
+    }
+}
